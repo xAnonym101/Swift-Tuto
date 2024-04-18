94c56748c13be3ec69ffddbf7245c0a736ab5311
Steven Gonawan <xvgonst@gmail.com>
2024-04-18 13:11:41 +0700

    Chapter 1.2 WIP

--- Landmarks/View/LandmarkRow.swift
+++ Landmarks/View/LandmarkRow.swift
@@ -1,5 +1,5 @@
 //
-//  MapView.swift
+//  LandmarkRow.swift
 //  Landmarks
 //
 //  Created by student on 18/04/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct MapView: View {
+struct LandmarkRow: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MapView()
+    LandmarkRow()
 }
