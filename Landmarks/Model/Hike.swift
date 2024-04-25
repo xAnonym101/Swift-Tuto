33cd64954782aeb61abaa056040ec02a8be60d3c
Steven Gonawan <xvgonst@gmail.com>
2024-04-25 11:26:21 +0700

    Chapter 2.2 DONE

---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,18 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by student on 25/04/24.
+//
+
+import SwiftUI
+
+struct Hike: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hike()
+}
