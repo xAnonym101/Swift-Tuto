cac9b5415c9ff9672f5072adf85eaf47afb69172
Steven Gonawan <xvgonst@gmail.com>
2024-05-02 13:10:39 +0700

    Chapter 4.2 [WATCHOS] DONE

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -10,10 +10,15 @@ import SwiftUI
 @main
 struct LandmarksApp: App {
     @State private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(modelData)
         }
+        
+        #if os(watchOS)
+                WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by student on 02/05/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(
+                    options: [.alert, .sound, .badge]
+                )
+            }
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
